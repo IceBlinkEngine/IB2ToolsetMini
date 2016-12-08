309f8e758966491150e834576e13105ff8fcd40c
slowdive-fan <slowdive.fam@gmail.com>
2016-12-07 22:02:02 -0700

    added props/triggers casting spell on their square upon entering creature/PC

---
+++ IB2ToolsetMini/bin/Debug/default/NewModule/scripts/gaPropOrTriggerCastSpellOnThisSquare.cs
@@ -0,0 +1,5 @@
+//gaPropOrTriggerCastSpellOnThisSquare.cs - Casts a spell at the trigger or props location in combat only
+//parm1 = (string) Spell tag
+//parm2 = none
+//parm3 = none
+//parm4 = none
