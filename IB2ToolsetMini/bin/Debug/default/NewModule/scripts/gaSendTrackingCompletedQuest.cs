b7bdcaa062765b6d8973a288db4ddd277b945f4f
slowdive-fan <slowdive.fam@gmail.com>
2017-02-19 12:18:49 -0700

    minor updates

---
+++ IB2ToolsetMini/bin/Debug/default/NewModule/scripts/gaSendTrackingCompletedQuest.cs
@@ -0,0 +1,5 @@
+//gaSendTrackingCompletedQuest.cs - Send a label for a main quest completed or important part of module like end of module. Sent to Developer's Google Analytics:
+//parm1 = (string) Action (ex. PartyEndingCh1, PartyDefeatedLizardQueen, etc.)
+//parm2 = none
+//parm3 = none
+//parm4 = none
